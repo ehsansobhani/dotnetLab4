88531c7a5dafaf42cf7ae711921bedf9630c98f0
ehsansobhani <e.sobhani2000@gmail.com>
2022-07-06 03:20:21 -0700

    Update Subscription.cs
    Relations are modified

--- Lab4/Models/Subscription.cs
+++ Lab4/Models/Subscription.cs
@@ -1,9 +1,14 @@
-﻿namespace Lab4.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab4.Models
 {
     public class Subscription
     {
-      public int ClientId { get; set; }
-      public string BrokerageId { get; set; }
-
+        
+        public int ClientId { get; set; }
+        public string BrokerageId { get; set; }
+        public virtual Client Client { get; set; }
+        public virtual Brokerage Brokerage { get; set; }
     }
 }
