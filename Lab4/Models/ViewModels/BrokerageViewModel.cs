890a00937f206acfe10881f022a440bfe45eb8c5
ehsansobhani <e.sobhani2000@gmail.com>
2022-07-06 03:21:14 -0700

    Update BrokerageViewModel.cs

--- Lab4/Models/ViewModels/BrokerageViewModel.cs
+++ Lab4/Models/ViewModels/BrokerageViewModel.cs
@@ -1,9 +1,10 @@
-﻿namespace Lab4.Models.ViewModels
+namespace Lab4.Models.ViewModels
 {
     public class BrokerageViewModel
     {
         public IEnumerable<Brokerage> Brokerages { get; set; }
         public IEnumerable<Client> Clients { get; set; }
         public IEnumerable<Subscription> Subscriptions { get; set; }
+
     }
 }
